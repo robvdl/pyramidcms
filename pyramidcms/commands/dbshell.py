068317e611ea0a700e0f21cb8ceb75862f7c37c1
Rob van der Linde <robvdl@gmail.com>
2015-01-10 17:05:48 +1300

    Re-arrange imports, use dict literals
    * Also add a few TODO's and FIXME's
    * Also specifically check if dbms is "sqlite" and else raise a CommandError
    * Using .startswith was not needed, as the :// was already removed as this point, so a simple == was enough
    * Don't call connection.get('dbms') excessively, store it in a variable

--- pyramidcms/commands/dbshell.py
+++ pyramidcms/commands/dbshell.py
@@ -1,40 +1,41 @@
-from pyramidcms.cli import BaseCommand
-
+import re
+import os
 from subprocess import call
 
-import os
-
-import re
+from pyramidcms.cli import BaseCommand
+from pyramidcms.exceptions import CommandError
 
 
 class Command(BaseCommand):
 
     def parse_url(self):
-        connection = dict()
+        connection = {}
+        # TODO: if sqlalchemy.url is missing it will crash, we should raise a CommandError
         parameters = re.split(r'[:@/]+', self.settings['sqlalchemy.url'])
-        connection.update(dict(dbms=parameters[0], username=parameters[1], host=parameters[3]))
+        connection.update({'dbms': parameters[0], 'username': parameters[1], 'host':parameters[3]})
         # Get the port number if available.
         if len(parameters) == 6:
-            connection.update(dict(port=parameters[4], database=parameters[5]))
+            connection.update({'port': parameters[4], 'database': parameters[5]})
         else:
-            connection.update(dict(database=parameters[4]))
+            connection.update({'database': parameters[4]})
         return connection
 
     def load_dbms_shell(self, connection):
-        if connection.get('dbms').startswith('mysql'):
-            command = 'mysql -u {username} -h {host} -D {database}'.format(username=connection.get('username'),
-                                                                           host=connection.get('host'),
-                                                                           database=connection.get('database'))
-        elif connection.get('dbms').startswith('postgresql'):
-            command = 'psql -U {username} -h {host} -d {database}'.format(username=connection.get('username'),
-                                                                          host=connection.get('host'),
-                                                                          database=connection.get('database'))
-        else:
-            command = 'sqlite3 ' + os.path.abspath(os.path.join(__file__, '../..')) + '/' +\
+        dbms = connection.get('dbms', '')
+        if dbms == 'mysql':
+            command = 'mysql -u {username} -h {host} -D {database}'.format(**connection)
+        elif dbms == 'postgresql':
+            command = 'psql -U {username} -h {host} -d {database}'.format(**connection)
+        elif dbms == 'sqlite':
+            # FIXME: this is not using os.path.dirname to strip off the filename of this .py file (__file__)
+            # FIXME: we shouldn't have to add .db, as the full filename of the database should be in the connection string
+            command = 'sqlite3 ' + os.path.abspath(os.path.join(__file__, '../..')) + '/' + \
                       connection.get('database') + '.db'
+        else:
+            raise CommandError('Unsupported DBMS ' + dbms)
 
         if 'port' in connection:
-            if connection.get('dbms').startswith('mysql'):
+            if dbms.startswith('mysql'):
                 port_arg = ' -P '
             else:
                 port_arg = ' -p '
