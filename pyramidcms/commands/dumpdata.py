610f52d7a1a0f072f5ee3b3cb660fe1357cae72b
Rob van der Linde <robvdl@gmail.com>
2015-01-12 20:37:08 +1300

    Rudimentary dumpdata command
    Dumps all models and records to as JSON sys.stdout
    
    There is no way to select a specific subset of models yet, or a way to customise the indentation (at the moment it's always set to 4)

---
+++ pyramidcms/commands/dumpdata.py
@@ -0,0 +1,25 @@
+import sys
+import json
+import argparse
+
+from pyramidcms.cli import BaseCommand
+from pyramidcms.db import models
+
+
+class Command(BaseCommand):
+    """
+    Dumps all models to JSON, can be piped into a file to generate fixtures.
+
+    Can also specify a list of models to dump a specific set.
+    """
+
+    def setup_args(self, parser):
+        parser.add_argument('models', type=str, nargs=argparse.REMAINDER,
+                            help='Optional list of models (will dump everything if omitted) ')
+
+    def handle(self, args):
+        # This nested list comprehension builds a data structure of all the
+        # models, and all records, which can then be converted to JSON.
+        json_data = [{'model': m.__name__, 'items': [obj.as_dict() for obj in m.objects.all()]} for m in models.__all__]
+        json.dump(json_data, sys.stdout, indent=4)
+        sys.stdout.write('\n')
