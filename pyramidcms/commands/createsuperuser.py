a27145b904d5b908a6b37d2571cc191c99e1b609
Rob van der Linde <robvdl@gmail.com>
2015-01-27 08:24:18 +1300

    Several improvements to createsuperuser
    * Don't make get_superuser_details a property
    * input() does not return None with no input, it returns an empty str
    * Validators class is a bit overkill to store regexes on, as we never construct this class
    * renamed utils.py to validators.py
    * docstrings were missing :param: and :return: description, they were only listing the arguments and not describing them

--- pyramidcms/commands/createsuperuser.py
+++ pyramidcms/commands/createsuperuser.py
@@ -1,7 +1,7 @@
 import getpass
-import re
+import transaction
 
-from pyramidcms.utils import Validators
+from pyramidcms.validators import RE_VALID_EMAIL
 from pyramidcms.cli import BaseCommand
 from pyramidcms.db.models import User
 
@@ -11,25 +11,23 @@ class Command(BaseCommand):
     Creates a superuser, prompting the user for input.
     """
 
-    @property
     def get_superuser_details(self):
         """
         Prompts for username, password and email. If no username is
         given then uses system user as default. Password must validate.
         Email can be blank.
 
-        :return: superuser_details
+        :return: a dict containing username, password and email
         """
-
-        username = input("Username: (default '" + getpass.getuser() + "')")
-        if username is None:
+        username = input("Username (default '" + getpass.getuser() + "'): ")
+        if username == '':
             username = getpass.getuser()
 
         valid_password = False
         password = ''
         while not valid_password:
-            password = getpass.getpass('Password:')
-            confirm_password = getpass.getpass('Repeat password:')
+            password = getpass.getpass('Password: ')
+            confirm_password = getpass.getpass('Repeat password: ')
             if password == '' or confirm_password == '':
                 print('Error: Password can not be blank.')
             elif password != confirm_password:
@@ -41,33 +39,32 @@ class Command(BaseCommand):
         email = ''
         while not valid_email:
             email = input('Email:')
-            match = re.search(Validators.validate_email, email)
+            match = RE_VALID_EMAIL.search(email)
             if match or email == '':
                 valid_email = True
             else:
                 print('Error: Enter a valid email address.')
 
-        superuser_details = {'username': username, 'password': password, 'email': email}
-
-        return superuser_details
+        return {
+            'username': username,
+            'password': password,
+            'email': email
+        }
 
     def create_superuser(self, superuser_details):
         """
         Creates a superuser in the database according to the parameters
         given in superuser_details.
 
-        :param superuser_details:
+        :param superuser_details: a dict containing username, password, email
         """
-
-        username = superuser_details['username']
-        user = User(username=username,
+        user = User(username=superuser_details['username'],
                     email=superuser_details['email'],
                     superuser=True)
         user.set_password(superuser_details['password'])
-        user.save()
-        if User.objects.get(username=username) is not None:
-            print('Superuser ' + username  + ' created.')
+        with transaction.manager:
+            user.save()
 
     def handle(self, args):
-        superuser_details = self.get_superuser_details
+        superuser_details = self.get_superuser_details()
         self.create_superuser(superuser_details)
