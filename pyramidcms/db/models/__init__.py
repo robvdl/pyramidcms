5872175e6c379168468596ef1de422d83f27aa22
Rob van der Linde <robvdl@gmail.com>
2015-02-02 09:17:30 +1300

    Fix typo and update comment

--- pyramidcms/db/models/__init__.py
+++ pyramidcms/db/models/__init__.py
@@ -1,8 +1,13 @@
 """
-All models in are be imported here and added to __all__.
+All models need to be imported here and added to __all__.
 
-This satisfies pyflakes static code analysis, but it also used to
-bootstrap the objects.model property for each model which is important.
+This both satisfies pyflakes static code analysis for unused imports,
+it is also used to provide an instance to each model on it's own manager
+class instance, objects. (it bootstraps Model.objects.model basically)
+
+We also need to import each model for alembic anyway, or they won't be
+included in migrations which is not what we want, so this is a way all
+models always get imported when you import anything from db.models.
 """
 
 import sys
