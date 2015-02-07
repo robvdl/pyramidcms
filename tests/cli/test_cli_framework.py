eb2293e5157034082b5400505e4366df7c4e7df0
Rob van der Linde <robvdl@gmail.com>
2015-02-08 09:05:46 +1300

    Extend test by checking exception case too

--- tests/cli/test_cli_framework.py
+++ tests/cli/test_cli_framework.py
@@ -2,6 +2,7 @@ import argparse
 from unittest import TestCase
 
 from pyramidcms import cli
+from pyramidcms.core.exceptions import CommandError
 
 
 class CliFrameworkTests(TestCase):
@@ -14,8 +15,15 @@ class CliFrameworkTests(TestCase):
         self.parser.add_argument('command', type=str, nargs=argparse.REMAINDER)
 
     def test_get_command(self):
+        # command should be the first argument
         args = self.parser.parse_args(['command1'])
         self.assertEqual(cli.get_command('pcms', args), 'command1')
 
+        # help followed by command should return first argument
         args = self.parser.parse_args(['help', 'command2'])
         self.assertEqual(cli.get_command('pcms', args), 'command2')
+
+        # help without a command should raise CommandError
+        args = self.parser.parse_args(['help'])
+        with self.assertRaises(CommandError):
+             cli.get_command('pcms', args)
