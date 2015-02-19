629f7b7105f32304b04fecbcdeeaa0de3c64f2fb
Rob van der Linde <robvdl@gmail.com>
2015-02-19 20:46:00 +1300

    Finish cli tests
    100% coverage :)
    
    closes #23

--- tests/cli/test_base_command.py
+++ tests/cli/test_base_command.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from pyramidcms import cli
 
@@ -36,3 +36,28 @@ class BaseCommandTests(TestCase):
         cli.BaseCommand('pcms', 'command2', mock_settings)
         self.assertFalse(engine_mock.called)
         self.assertFalse(session_mock.called)
+
+    def test_run(self):
+        """
+        A simple test that ensures the run method calls handle() in the
+        command class that inherits BaseCommand.
+        """
+        command = cli.BaseCommand('pcms', 'command1', {})
+
+        # create mock and pass through to original method for coverage
+        command.handle = Mock(wraps=command.handle)
+        command.run()
+
+        # the run method is expected to call handle()
+        self.assertTrue(command.handle.called)
+
+    def test_help(self):
+        """
+        Basic test to check the help() method, mostly just for coverage,
+        but it basically it checks that the help() method calls print_help()
+        on the argparse object for this command.
+        """
+        command = cli.BaseCommand('pcms', 'command1', {})
+        command.parser = Mock()
+        command.help()
+        self.assertTrue(command.parser.print_help.called)
