847d9fe1abbd49c4d457c5223c108c68fac9c6aa
Rob van der Linde <robvdl@gmail.com>
2015-05-03 21:59:59 +1200

    Basic collectstatic command
    * Only copies files that have actually changed
    * Can recursively copy files and create subdirectories
    
    Things not covered yet:
    
    * A command line option to empty the collecttion dir before running collectstatic
    * A command line option to force overwriting files, even if the timestamps are the same (is this useful or not?)
    
    The command could probably do with some improved error handling too.

---
+++ pyramidcms/commands/collectstatic.py
@@ -0,0 +1,81 @@
+import os
+import shutil
+
+from pyramidcms.cli import BaseCommand
+from pyramidcms.config import get_static_dirs
+
+
+class Command(BaseCommand):
+    """
+    Find all static files in directories from the setting static.dirs and
+    copies them into the static.collection_dir folder.
+    """
+
+    def copy_files(self, src, dest):
+        """
+        Copy files and folders recursively from the source directory
+        to the destination.
+
+        If a file is already in the destination folder, the file timestamps
+        are compared to determine if the file needs to be copied again,
+        or if we can skip copying that file.
+
+        :param src: source directory, copies files and folders inside.
+        :param dest: destination directory, will be created if doesn't exist.
+        :return: (total_files, num_files_copied, num_dirs_created)
+        :rtype: tuple
+        """
+        total_files, num_files_copied, num_dirs_created = 0, 0, 0
+
+        if not os.path.exists(dest):
+            print('Create directory: "{}"'.format(dest))
+            os.makedirs(dest)
+            num_dirs_created += 1
+
+        for root, dirs, files in os.walk(src):
+            for item in files:
+                src_path = os.path.join(root, item)
+                dst_path = os.path.join(dest, src_path.replace(src, ''))
+
+                if os.path.exists(dst_path):
+                    if os.stat(src_path).st_mtime > os.stat(dst_path).st_mtime:
+                        print('Copying: "{}" => "{}"'.format(src_path, dst_path))
+                        shutil.copy2(src_path, dst_path)
+                        num_files_copied += 1
+                else:
+                    print('Copying: "{}" => "{}"'.format(src_path, dst_path))
+                    shutil.copy2(src_path, dst_path)
+                    num_files_copied += 1
+
+                total_files += 1
+
+            for item in dirs:
+                src_path = os.path.join(root, item)
+                dst_path = os.path.join(dest, src_path.replace(src, ''))
+                if not os.path.exists(dst_path):
+                    print('Create directory: "{}"'.format(dst_path))
+                    os.mkdir(dst_path)
+                    num_dirs_created += 1
+
+        return total_files, num_files_copied, num_dirs_created
+
+    def handle(self, args):
+        static_dirs = get_static_dirs(self.settings)
+        collect_dir = self.settings.get('static.collect_dir')
+        print('Source directories: "{}"'.format('", "'.join(static_dirs)))
+        print('Destination directory: "{}"\n'.format(collect_dir))
+
+        total_files, num_files_copied, num_dirs_created = 0, 0, 0
+        for directory in static_dirs:
+            total, num_copied, dirs_created = self.copy_files(directory, collect_dir)
+            total_files += total
+            num_files_copied += num_copied
+            num_dirs_created += dirs_created
+
+        # only print a newline if there was output during copying
+        if num_dirs_created > 0 or num_files_copied > 0:
+            print('\n')
+
+        print('Number of files copied: {}'.format(num_files_copied))
+        print('Number of directories created: {}'.format(num_dirs_created))
+        print('Number of files un-modified: {}'.format(total_files - num_files_copied))
