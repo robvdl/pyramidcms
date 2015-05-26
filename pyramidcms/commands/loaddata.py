6f981b958c0107d8694293933e2a3d1d897f28e4
Rob van der Linde <robvdl@gmail.com>
2015-05-26 22:55:18 +1200

    Update the test data to match api ACLAuthorization class

--- pyramidcms/commands/loaddata.py
+++ pyramidcms/commands/loaddata.py
@@ -13,13 +13,39 @@ class Command(BaseCommand):
 
     def handle(self, args):
         with transaction.manager:
-            perm_create = Permission.objects.create(name='create', description='User can create item')
-            perm_edit = Permission.objects.create(name='edit', description='User can edit content')
-            perm_delete = Permission.objects.create(name='delete', description='User can delete items')
-            perm_view = Permission.objects.create(name='view', description='User can view item')
-            g1 = Group.objects.create(name='Group 1', permissions=[perm_create])
-            g2 = Group.objects.create(name='Group 2', permissions=[perm_delete, perm_create, perm_view])
-            g3 = Group.objects.create(name='Group 3', permissions=[perm_create, perm_edit])
+            # permissions
+            perms = {}
+
+            # load some permissions for the API resource ACLAuthorization class
+            for resource in ('user', 'group', 'permission'):
+                perms[resource] = {
+                    'create': Permission.objects.create(name='create-' + resource, description='User can create {} resources.'.format(resource)),
+                    'read': Permission.objects.create(name='read-' + resource, description='User can read {} resources.'.format(resource)),
+                    'update': Permission.objects.create(name='update-' + resource, description='User can update {} resources.'.format(resource)),
+                    'delete': Permission.objects.create(name='delete-' + resource, description='User can delete {} resources.'.format(resource)),
+                }
+
+            # groups
+            g1 = Group.objects.create(name='Group 1', permissions=[
+                perms['user']['create'],
+                perms['user']['read'],
+                perms['user']['update'],
+                perms['user']['delete'],
+            ])
+            g2 = Group.objects.create(name='Group 2', permissions=[
+                perms['group']['create'],
+                perms['group']['read'],
+                perms['group']['update'],
+                perms['group']['delete'],
+            ])
+            g3 = Group.objects.create(name='Group 3', permissions=[
+                perms['permission']['create'],
+                perms['permission']['read'],
+                perms['permission']['update'],
+                perms['permission']['delete'],
+            ])
+
+            # users
             admin_user = User(username='admin', is_superuser=True, groups=[g1, g2])
             admin_user.set_password('admin')
             admin_user.save()
