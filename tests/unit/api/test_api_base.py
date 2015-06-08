f9bf19841efed2b6e80a77560c02af544470b5f5
Rob van der Linde <robvdl@gmail.com>
2015-06-08 23:49:54 +1200

    Remove unused import, was failing flake8

--- tests/unit/api/test_api_base.py
+++ tests/unit/api/test_api_base.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch, Mock, MagicMock, PropertyMock
+from unittest.mock import patch, Mock, MagicMock
 
 from pyramid import testing
 from pyramid.httpexceptions import HTTPBadRequest, HTTPForbidden, HTTPNotFound,\
